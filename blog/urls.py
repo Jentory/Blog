8e4ea8f376069e1a9030bbd05cb6aa54d69c4c85
zhenzhen.hou03 <zhenzhen.hou03@ele.me>
2019-05-09 17:32:04 +0800

    优化-20190509

--- blog/urls.py
+++ blog/urls.py
@@ -7,7 +7,7 @@ app_name = 'blog'
 urlpatterns = [
     path('index', views.index),
     # re_path(r'^article/(?P<article_id>[0-9]+)$', views.article_page),
-    path('article/<article_id>', views.article_page, name='article_page'),
-    path('edit/<article_id>', views.edit_page, name='edit_page'),
-    path('edit/action', views.edit_action, name='edit_action'),
+    path('article/<int:article_id>', views.article_page, name='article_page'),
+    path('edit/<int:article_id>', views.edit_page, name='edit_page'),
+    path('editaction', views.edit_action, name='edit_action'),
 ]
